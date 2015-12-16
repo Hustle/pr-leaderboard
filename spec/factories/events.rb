ba6943eed9c73e24f9b955074eb3fb83879bca66
Karl Baum <karl.baum@gmail.com>
2015-12-16 18:18:45 -0500

    added pull request payload

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -6,30 +6,15 @@ FactoryGirl.define do
   end
 
   factory :user_data, parent: :data do
-    data {
-      {
-        "id"=>96786,
-        "url"=>"https://api.github.com/users/kbaum",
-        "type"=>"User",
-        "login"=>"kbaum",
-        "html_url"=>"https://github.com/kbaum",
-        "gists_url"=>"https://api.github.com/users/kbaum/gists{/gist_id}",
-        "repos_url"=>"https://api.github.com/users/kbaum/repos",
-        "avatar_url"=>"https://avatars.githubusercontent.com/u/96786?v=3",
-        "events_url"=>"https://api.github.com/users/kbaum/events{/privacy}",
-        "site_admin"=>false,
-        "gravatar_id"=>"",
-        "starred_url"=>"https://api.github.com/users/kbaum/starred{/owner}{/repo}",
-        "followers_url"=>"https://api.github.com/users/kbaum/followers",
-        "following_url"=>"https://api.github.com/users/kbaum/following{/other_user}",
-        "organizations_url"=>"https://api.github.com/users/kbaum/orgs",
-        "subscriptions_url"=>"https://api.github.com/users/kbaum/subscriptions",
-        "received_events_url"=>"https://api.github.com/users/kbaum/received_events"
-      }
-    }
-    initialize_with { Hashie::Mash.new(data) }
+    id { SecureRandom.uuid }
+    type "User"
+    login "kbaum"
+    avatar_url "https://avatars.githubusercontent.com/u/96786?v=3"
   end
 
+  factory :pull_request_payload_data, parent: :data do
+    user { create :user_data }
+  end
 
 
   factory :event_data, parent: :data do
@@ -39,12 +24,16 @@ FactoryGirl.define do
 
   factory :payload, parent: :data do
     action 'closed'
-    pull_request
+    pull_request { create :pull_request_payload_data }
   end
 
   factory :merged_pull_request_data, parent: :event_data do
     type 'PullRequestEvent'
     payload
+  end
+
+  factory :merged_pull_request, class: Event do
+    data { create :merged_pull_request_data }
   end
 
 
@@ -53,15 +42,9 @@ FactoryGirl.define do
   end
 
 
-  factory :pull_request, class: Event do
-    data { create :pull_request_data}
-  end
-
   factory :pull_request_comment, class: Event do
     data { create :pull_request_comment_data }
   end
-
-
 
 
 
