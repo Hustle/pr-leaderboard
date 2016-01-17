c57d5df9df03108af3603dfcc9e57708ed9b6358
Karl Baum <karl.baum@gmail.com>
2016-01-17 12:52:26 -0500

    leaderboard controller passes dates to leader board

--- app/controllers/leader_board_controller.rb
+++ app/controllers/leader_board_controller.rb
@@ -1,9 +1,17 @@
 class LeaderBoardController < ApplicationController
   def show
     if request.format.html?
-      @results = LeaderBoard.new(Date.today).results
+      @results = LeaderBoard.new(date).results
     else
-      render json: LeaderBoard.new(Date.today).results
+      render json: LeaderBoard.new(date).results
     end
   end
+
+
+  private
+
+  def date
+    Date.parse(params[:date].presence || Date.today.to_s)
+  end
+
 end
--- spec/controllers/leader_board_controller_spec.rb
+++ spec/controllers/leader_board_controller_spec.rb
@@ -1,6 +1,10 @@
-require 'spec_helper'
+require 'rails_helper'
 
 describe LeaderBoardController do
+
+  before do
+    Timecop.freeze
+  end
 
 
   describe 'Returns leader board results for today if not date specified' do
@@ -14,7 +18,37 @@ describe LeaderBoardController do
       get :show
     end
 
+  end
+
+  describe 'Returns leader board results for today if not date specified' do
+
+    before do
+      expect(LeaderBoard).to receive(:new).with(Date.today).and_return(double(:leader_board, results: []))
+    end
+
+    specify do
+      get :show
+    end
 
   end
+
+  describe 'Returns leader board for a date passed in' do
+
+    let(:date){ 3.days.ago.to_date }
+
+    before do
+      expect(LeaderBoard).to receive(:new).with(date).and_return(double(:leader_board, results: []))
+    end
+
+    specify do
+      get :show, date: date.to_s
+    end
+
+
+  end
+
+
+
+
 
 end
