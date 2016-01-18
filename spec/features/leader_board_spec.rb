f3724eb751ef81a730b3042a0963ce5eadf55f03
Karl Baum <karl.baum@gmail.com>
2016-01-18 10:38:11 -0500

    added tests to ensure we can see the previous sprints

--- spec/features/leader_board_spec.rb
+++ spec/features/leader_board_spec.rb
@@ -11,5 +11,29 @@ describe 'leaderboard' do
   end
 
 
+  describe 'pass in dates for previous sprints' do
+
+    before do
+      Sprint.start_date = Date.parse('2016-01-04')
+      Timecop.freeze '2016-01-18'
+    end
+
+    specify 'visit for current sprint' do
+
+      visit '/'
+
+      expect(page).to have_content "Sprint 2016-01-18"
+
+    end
+
+    specify 'pass a date in to see previous sprint' do
+      visit root_path(date: '2016-01-06')
+
+      expect(page).to have_content "Sprint 2016-01-04"
+    end
+
+
+  end
+
 
 end
