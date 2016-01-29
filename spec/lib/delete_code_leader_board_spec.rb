0eb7b40984980d8fd505a640f582db5fb967806f
Karl Baum <karl.baum@gmail.com>
2016-01-29 13:53:07 +0000

    added delete code leader board basic spec

---
+++ spec/lib/delete_code_leader_board_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+describe DeleteCodeLeaderBoard do
+
+  let(:charley){ create(:github_user, data: create(:user_data, login: "CharlesMcMillan")) }
+  let(:ian){ create(:github_user, data: create(:user_data, login: "ianforsyth")) }
+
+  let(:date){ Date.parse('2015-12-17')   }
+
+
+  before do
+    #first sprint is
+    Sprint.start_date = Time.zone.parse('2015-11-23')
+
+    #currently Dec 17th so sprint started on the 7th
+    Timecop.freeze date
+
+    create(:merged_pull_request, data: build(:merged_pull_request_data, created_at: '2015-12-16',
+                                             payload: create(:payload, pull_request: build(:pull_request_payload_data, user: ian.data, deletions: 10 )) ))
+
+    create(:merged_pull_request, data: build(:merged_pull_request_data, created_at: '2015-12-16',
+                                             payload: create(:payload, pull_request: build(:pull_request_payload_data, user: charley.data, deletions: 5 )) ))
+
+    create(:merged_pull_request, data: build(:merged_pull_request_data, created_at: '2015-12-16',
+                                             payload: create(:payload, pull_request: build(:pull_request_payload_data, user: charley.data, deletions: 2 )) ))
+  end
+
+  specify do
+    expect(DeleteCodeLeaderBoard.new(date).results).to eq (
+      [
+        {
+          "login"=>"ianforsyth",
+          "deletions"=>10,
+          "github_user"=> ian
+        },
+        {
+          "login"=>"CharlesMcMillan",
+          "deletions"=>7,
+          "github_user"=> charley
+        }
+      ]
+    )
+  end
+
+
+end
