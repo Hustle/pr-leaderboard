276bf6f7ad1ffbae61fc999ed120184e90ce2ca9
Ken Liu <ken.liu@gmail.com>
2016-08-29 08:40:03 -0400

    cleanup

--- app/controllers/github_webhooks_controller.rb
+++ app/controllers/github_webhooks_controller.rb
@@ -1,14 +1,11 @@
 # app/controllers/github_webhooks_controller.rb
 class GithubWebhooksController < ActionController::Base
 
-
   include GithubWebhook::Processor
-
 
   def show
     render json: 'OK'
   end
-
 
   def create
     Rails.logger.info("Received event #{json_body}")
